49f95c74734ab67a7b70a3ee9e3fa40f7ce25a2e
guillermoroman <guillermoroman@outlook.com>
2024-02-05 08:07:24 +0100

    First commit

---
+++ src/Concesionario.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Concesionario {
+
+
+    private ArrayList<Auto> autos;
+
+
+    public Concesionario() {
+        autos = new ArrayList<>();
+    }
+
+
+    public void agregarAuto(Auto auto) {
+        autos.add(auto);
+    }
+
+
+    public ArrayList<Auto> listarAutos() {
+        return autos;
+    }
+
+
+    public void imprimirAutos(){
+        for (Auto auto: autos){
+            System.out.println(auto);
+        }
+    }
+}
+
